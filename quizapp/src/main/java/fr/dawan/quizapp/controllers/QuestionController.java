ec5514783140023b55e3953da4778804460e6823
admin stagiaire <admin stagiaire@a4bb6d7551a7>
2022-03-07 00:12:36 +0100

    yes

--- quizapp/src/main/java/fr/dawan/quizapp/controllers/QuestionController.java
+++ quizapp/src/main/java/fr/dawan/quizapp/controllers/QuestionController.java
@@ -8,8 +8,9 @@ import org.springframework.ui.ModelMap;
 
 import fr.dawan.quizapp.entities.Question;
 import fr.dawan.quizapp.services.QuestionService;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
@@ -19,7 +20,7 @@ public class QuestionController {
 	@Autowired
 	private QuestionService questionService;
 	
-	@RequestMapping( value="/index", method = RequestMethod.GET )
+	@GetMapping( value="/index")
 	 public String index(ModelMap modelMap) {
 		List<Question> listQuestion = questionService.findAll();
 		modelMap.addAttribute("questions", listQuestion);
